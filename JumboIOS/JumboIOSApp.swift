a0b46ec805d40b8938b851a0807decf54fd19f2b
Brennan <brennanbesser@gmail.com>
2026-01-28 12:35:12 -0600

    Initial Commit

---
+++ JumboIOS/JumboIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumboIOSApp.swift
+//  JumboIOS
+//
+//  Created by Brennan Besser on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct JumboIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
